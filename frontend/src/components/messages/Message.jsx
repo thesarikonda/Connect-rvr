2fadaf47c0a808bf89aad386059e154b721c5f7d
thesarikonda <140501081+thesarikonda@users.noreply.github.com>
2024-07-22 21:54:33 +0530

    2nd part completed - ui design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,18 @@
+
+
+const Message = () => {
+
+
+	return (
+		<div className='chat chat-end'>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={"https://avatar.iran.liara.run/public/boy"} />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white bg-blue-500`}>  Hi, whats up</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>10:30</div>
+		</div>
+	);
+};
+export default Message;
